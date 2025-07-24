9ba0b55cf6084c9349153670d3937956423d52e2
Felix Tse <ftse7+seneca@myseneca.ca>
2025-07-24 10:41:24 -0400

    setting up astrodb

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
   output: "static",
   build: {
     inlineStylesheets: "always",
